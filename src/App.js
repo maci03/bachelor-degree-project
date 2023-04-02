aee9dc3b2904fbfedc304c17e6b0dfd958c47d6b
მაცი <93665709+maci03@users.noreply.github.com>
2023-04-02 15:57:14 +0400

    work on structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
+import PlaceDetails from "./Components/PlaceDetails/PlaceDetails";
 
 
 const App = () => {
   return (
-    <div>
-      <h1>HEllo wolrd</h1>
-    </div>
+    <>
+    <CssBaseline />
+    <Header />
+    <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+      </Grid>
+      </Grid>
+
+    </>
   );
 }
 
